c6f516b0e09bea7c23f3ed76fab19830bb2a6b06
Malikhussainahmed-pixel <malikhussainahmed90@gmail.com>
2025-09-14 15:15:59 +0500

    Added padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body  className={lato.className}
+      <body  className={`${lato.className} px-4`}
       >
         <Navbar />
         {children}
